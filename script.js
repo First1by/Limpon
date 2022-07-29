974fd745798b783dcdb8f6188c4758278cd4bfe2
Sergey <77577095+First1by@users.noreply.github.com>
2022-07-29 15:53:46 +0300

    docs: add js momentum

---
+++ script.js
@@ -0,0 +1,47 @@
+const time = document.querySelector('.time');
+const data = document.querySelector('.date');
+const date = new Date();
+const hours = date.getHours();
+const currentTime = date.toLocaleTimeString();
+const options = {weekday: 'long', month: 'long', day: 'numeric'};
+const currentDate = date.toLocaleDateString('en-EN', options);
+const greeting = document.querySelector('.greeting');
+const timeOfDay = getTimeOfDay();
+const greetingText = `Good ${timeOfDay}`;
+
+showTime();
+
+
+function showTime() {
+  time.innerHTML = new Date().toLocaleTimeString();
+  showDate();
+  showGreeting();
+  setTimeout(showTime, 1000);
+}
+
+function showDate() {
+  data.innerHTML = new Date().toLocaleDateString('en-EN', options);
+}
+
+function getTimeOfDay() {
+  switch(Math.floor(hours/6)) {
+    case 0:
+    	return 'night'
+    break;
+    case 1:
+    	return 'morning'
+    break;
+    case 2:
+    	return 'day'
+    break;
+    case 3:
+    	return 'evening'
+    break;
+  }
+}
+
+function showGreeting () {
+  greeting.innerHTML = greetingText;
+}
+
+
